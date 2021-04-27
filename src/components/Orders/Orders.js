0d3d7b45d0d277943686b7b44f5c0db8a409c9ed
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-27 11:25:09 -0600

    Add styling to order cards

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -5,7 +5,7 @@ const Orders = props => {
   const orderEls = props.orders.map((order, i) => {
     return (
       <div key={i} className="order" data-cy="order-card">
-        <h3>{order.name}</h3>
+        <h3 className="order-name">{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map((ingredient, i) => {
             return <li key={i}>{ingredient}</li>
@@ -16,7 +16,7 @@ const Orders = props => {
   });
 
   return (
-    <section>
+    <section className="orders">
       { orderEls.length ? orderEls : <p>No orders yet!</p> }
     </section>
   )
